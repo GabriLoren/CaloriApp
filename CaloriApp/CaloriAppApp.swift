95636acc951d2c8bd28152668d8389d5c95a4de3
gabriloren <gabriel.lg90@gmail.com>
2024-10-18 12:57:30 +0200

    Initial Commit

---
+++ CaloriApp/CaloriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriAppApp.swift
+//  CaloriApp
+//
+//  Created by gabriel lorenzo on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CaloriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
